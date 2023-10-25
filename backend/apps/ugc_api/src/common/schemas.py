ca0779eb2064f8cbd461ef68f281d0995702a506
nikita zigman <zigman.nikita@gmail.com>
2023-10-25 21:30:34 +0100

    feat: adds common componens to the repo

---
+++ backend/apps/ugc_api/src/common/schemas.py
@@ -0,0 +1,22 @@
+import orjson
+from pydantic import BaseModel, Field, validator
+from pymongo.collection import ObjectId
+
+
+def orjson_dumps(value, *, default):
+    return orjson.dumps(value, default=default).decode()
+
+
+class BaseSchema(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class BaseMongoSchema(BaseSchema):
+    id: str | None = Field(alias="_id")
+
+    @validator("id", pre=True, always=True)
+    @classmethod
+    def convert_to_str(cls, _id: ObjectId) -> str | None:
+        return _id and str(_id)
