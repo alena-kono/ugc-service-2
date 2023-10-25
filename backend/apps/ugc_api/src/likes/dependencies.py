c5c8e611c3c8ba4515cfd6e5afa9c793c133f239
nikita zigman <zigman.nikita@gmail.com>
2023-10-25 21:31:16 +0100

    feat: adds likes domen

---
+++ backend/apps/ugc_api/src/likes/dependencies.py
@@ -0,0 +1,7 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.likes.services import ILikeService, get_service
+
+LikeServiceType = Annotated[ILikeService, Depends(get_service)]
