a637e76a81ad0b03ba27aa7427c2a877df0a944d
nikita zigman <zigman.nikita@gmail.com>
2023-10-25 21:31:40 +0100

    feat: registrate new routers to the ugc app

--- backend/apps/ugc_api/src/main.py
+++ backend/apps/ugc_api/src/main.py
@@ -3,19 +3,21 @@ from contextlib import asynccontextmanager
 from typing import Awaitable, Callable
 
 import uvicorn
+from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse, Response
 from fastapi_limiter import FastAPILimiter
+from fastapi_pagination import add_pagination
+from motor.motor_asyncio import AsyncIOMotorClient
 from opentelemetry import trace
 from redis import asyncio as aioredis
+from starlette.middleware.sessions import SessionMiddleware
+
+from src.common import databases
+from src.film_progress.api.v1.routers import router as film_progress_router
+from src.likes.api.v1.routers import router as likes_router
 from src.settings.app import get_app_settings
 from src.tracer.config import configure_tracer
-from src.utils import databases
-from src.ugc.api.v1.routers import router
-from starlette.middleware.sessions import SessionMiddleware
-from aiokafka import AIOKafkaProducer
-from motor.motor_asyncio import AsyncIOMotorClient
-
 
 settings = get_app_settings()
 logging.config.dictConfig(settings.logging.config)
@@ -88,12 +90,15 @@ async def setup_request_id_for_jaeger(
     return await call_next(request)
 
 
-app.include_router(router, prefix="/api/v1/ugc_event", tags=["ugc"])
+app.include_router(film_progress_router, prefix="/api/v1", tags=["film_progress"])
+app.include_router(likes_router, prefix="/api/v1", tags=["likes"])
 
 app.add_middleware(SessionMiddleware, secret_key=settings.auth.secret_key)
 
 if settings.jaeger.enabled:
     configure_tracer(app)
+
+add_pagination(app)
 
 if __name__ == "__main__":
     uvicorn.run(
