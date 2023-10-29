3a2b44ed55d97881c3b564a65371b6e8ed907084
alena-kono <alena.kono@fastmail.com>
2023-10-29 19:04:59 +0300

    feat: set up demo logging to console and json file using `structlog`

--- backend/apps/auth/src/main.py
+++ backend/apps/auth/src/main.py
@@ -2,6 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 from typing import Awaitable, Callable
 
+import structlog
 import uvicorn
 from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse, Response
@@ -15,13 +16,18 @@ from src.auth.api.v1 import router as auth_router
 from src.common import database
 from src.permissions.api.v1 import router as permissions_router
 from src.settings.app import get_app_settings
+from src.settings.logging import configure_logger
 from src.social.api.v1 import router as social_router
 from src.tracer.config import configure_tracer
 from src.users.api.v1 import router as users_router
 from starlette.middleware.sessions import SessionMiddleware
 
 settings = get_app_settings()
+
 logging.config.dictConfig(settings.logging.config)
+configure_logger()
+
+logger = structlog.get_logger()
 
 
 @asynccontextmanager
@@ -49,6 +55,7 @@ app = FastAPI(
 
 @app.get("/ping")
 def pong() -> dict[str, str]:
+    logger.info("pong")
     return {"ping": "pong!"}
 
 
@@ -64,6 +71,22 @@ async def check_request_id(
                 content={"detail": "X-Request-Id is required"},
             )
     return await call_next(request)
+
+
+@app.middleware("http")
+async def logging_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    structlog.contextvars.clear_contextvars()
+
+    structlog.contextvars.bind_contextvars(
+        request_id=request.headers.get("X-Request-Id"),
+        service_name=settings.service.name,
+    )
+
+    response: Response = await call_next(request)
+
+    return response
 
 
 @app.middleware("http")
--- backend/apps/auth/src/settings/logging.py
+++ backend/apps/auth/src/settings/logging.py
@@ -1,7 +1,10 @@
+import datetime
+import logging
 import typing as tp
 from enum import Enum
 
 import pydantic
+import structlog
 from src.settings.base import BaseAppSettings
 
 
@@ -17,63 +20,80 @@ class LoggingSettings(BaseAppSettings):
     class Config:
         use_enum_values = True
 
-    fmt: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    default_handlers: list[str] = ["console"]
     level: LoggerLevelType = pydantic.Field(
         env="LOGGING_LEVEL", default=LoggerLevelType.DEBUG
     )
+    json_log_file_path: str = pydantic.Field(default="../../../logs/apps/auth.log")
 
     @property
     def config(self) -> dict[str, tp.Any]:
         return {
             "version": 1,
-            "disable_existing_loggers": False,
+            "disable_existing_loggers": True,
             "formatters": {
-                "verbose": {"format": self.fmt},
-                "default": {
-                    "()": "uvicorn.logging.DefaultFormatter",
-                    "fmt": "%(levelprefix)s %(message)s",
-                    "use_colors": None,
+                "json_formatter": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.processors.JSONRenderer(),
                 },
-                "access": {
-                    "()": "uvicorn.logging.AccessFormatter",
-                    "fmt": "%(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s",  # noqa: E501
+                "plain_console": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(),
                 },
             },
             "handlers": {
                 "console": {
                     "level": self.level,
                     "class": "logging.StreamHandler",
-                    "formatter": "verbose",
+                    "formatter": "plain_console",
                 },
-                "default": {
-                    "formatter": "default",
-                    "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",
-                },
-                "access": {
-                    "formatter": "access",
-                    "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",
+                "json_file": {
+                    "level": self.level,
+                    "class": "logging.handlers.TimedRotatingFileHandler",
+                    "when": "midnight",
+                    "atTime": datetime.time(hour=0),
+                    "interval": 1,
+                    "backupCount": 2,
+                    "filename": self.json_log_file_path,
+                    "formatter": "json_formatter",
                 },
             },
             "loggers": {
                 "": {
-                    "handlers": self.default_handlers,
+                    "handlers": ["console", "json_file"],
                     "level": self.level,
-                },
-                "uvicorn.error": {
-                    "level": self.level,
-                },
-                "uvicorn.access": {
-                    "handlers": ["access"],
-                    "level": self.level,
-                    "propagate": False,
-                },
-            },
-            "root": {
-                "level": self.level,
-                "formatter": "verbose",
-                "handlers": self.default_handlers,
+                }
             },
         }
+
+
+def configure_logger() -> None:
+    """Configure structlog logger."""
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.filter_by_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.stdlib.ExtraAdder(),
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ]
+
+    structlog.configure(
+        processors=shared_processors,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    _clear_log_handlers(["uvicorn.access", "uvicorn.error"])
+
+
+def _clear_log_handlers(loggers_names: list[str]) -> None:
+    """Clear the log handlers for loggers, and enable propagation."""
+    for _log in loggers_names:
+        logging.getLogger(_log).handlers.clear()
+        logging.getLogger(_log).propagate = True
